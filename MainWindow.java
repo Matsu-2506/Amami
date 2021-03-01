076ad9ce57c8de53f24b424f63942f9159a274c7
MiyuMatsui <g18938mm@gm.tsuda.ac.jp>
2021-03-01 09:18:21 +0900

    メインのWIndow作成

---
+++ MainWindow.java
@@ -0,0 +1,9 @@
+import javax.swing.JFrame;
+
+class JSample1_1{
+  public static void main(String args[]){
+    JFrame frame = new JFrame();
+    frame.setTitle("MyTitle");
+    frame.setVisible(true);
+  }
+}
