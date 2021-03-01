39b7b9b10835da52aff8189100177eee1cac5a86
MiyuMatsui <g18938mm@gm.tsuda.ac.jp>
2021-03-01 09:22:17 +0900

    WIndowサイズの変更

--- MainWindow.java
+++ MainWindow.java
@@ -4,6 +4,7 @@ class JSample1_1{
   public static void main(String args[]){
     JFrame frame = new JFrame();
     frame.setTitle("MyTitle");
+    frame.setSize(600, 400);
     frame.setVisible(true);
   }
 }
