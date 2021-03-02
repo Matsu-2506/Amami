044438dce276d16638a0183e758db3684bc9fd09
MiyuMatsui <g18938mm@gm.tsuda.ac.jp>
2021-03-02 09:37:47 +0900

    MainWindow修正

--- MainWindow.java
+++ MainWindow.java
@@ -5,16 +5,13 @@ import java.awt.FlowLayout;
 class MainWindow{
   public static void main(String args[]){
     JFrame frame = new JFrame();
-    frame.setTitle("DrawToolWindow");
+    
 
     //Windowã®é€æ˜åŒ– ã§ããªã„?
-    //Window‚Ì“§–¾‰» ‚Å‚«‚È‚¢?
     //JPanel panel = new JPanel();
     //panel.setOpaque(false);
     //ãƒŸãƒ¼ãƒ†ã‚£ãƒ³ã‚°ã‚¢ãƒ—ãƒªwindowã®ä½ç½®ã‚’å–å¾—
-    //ƒ~[ƒeƒBƒ“ƒOƒAƒvƒŠwindow‚ÌˆÊ’u‚ğæ“¾
 
-<<<<<<< HEAD
     //windowã®ã‚µã‚¤ã‚ºã€ä½ç½®ã®è¨­å®š
     //frame.setSize(600, 400);
     //frame.setLocation(100, 100);
@@ -26,13 +23,11 @@ class MainWindow{
     // frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     // frame.getContentPane().setLayout(new FlowLayout());
     // frame.setVisible(true);
-=======
-    //window‚ÌƒTƒCƒYAˆÊ’u‚Ìİ’è
-    frame.setSize(600, 400);
+   
+    // frame.setSize(600, 400);
     //frame.setLocation(100, 100);
 
-    //window‚Ì•\¦
-    frame.setVisible(true);
->>>>>>> master
+
+    // frame.setVisible(true);
   }
 }
