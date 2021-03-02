426c4170672da935d1a274f888ce69d1f266a3c5
MiyuMatsui <g18938mm@gm.tsuda.ac.jp>
2021-03-02 12:26:37 +0900

    no message

--- MainWindow.java
+++ MainWindow.java
@@ -5,35 +5,10 @@ import java.awt.FlowLayout;
 class MainWindow{
   public static void main(String args[]){
     JFrame frame = new JFrame();
-    
+    frame.setTitle("DrawToolWindow");
 
-<<<<<<< HEAD
-    //Windowの透明化 できない?
-    //JPanel panel = new JPanel();
-    //panel.setOpaque(false);
-    //ミーティングアプリwindowの位置を取得
-
-    //windowのサイズ、位置の設定
-    //frame.setSize(600, 400);
-    //frame.setLocation(100, 100);
-
-    //windowの表示
-    //frame.setVisible(true);
-
-    CaptureAndDraw cad = new CaptureAndDraw("Capture Test");
-    // frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    // frame.getContentPane().setLayout(new FlowLayout());
-    // frame.setVisible(true);
-   
-    // frame.setSize(600, 400);
-    //frame.setLocation(100, 100);
-
-
-    // frame.setVisible(true);
-=======
     CaptureAndDraw cad = new CaptureAndDraw("Capture Test");
    
 
->>>>>>> master
   }
 }
